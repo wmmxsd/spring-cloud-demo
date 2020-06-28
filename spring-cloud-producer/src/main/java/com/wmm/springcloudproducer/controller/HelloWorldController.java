71b21cc416535f8c03df74602137c458337d0694
wmmxsd@126.com <w7610232mm>
2020-06-28 15:24:13 +0800

    服务提供方demo

---
+++ spring-cloud-producer/src/main/java/com/wmm/springcloudproducer/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.wmm.springcloudproducer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangmingming
+ * @Description HelloWorld控制层
+ * @date @2020/6/28 15:03
+ */
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello " + name + "，this is first message";
+    }
+}
