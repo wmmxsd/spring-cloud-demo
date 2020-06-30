32ae92014bf6d5312b52ee2a208f67d1a1082609
wmmxsd@126.com <w7610232mm>
2020-06-30 10:27:37 +0800

    add spring-cloud-config project

---
+++ spring-cloud-config/src/main/java/com/wmm/springcloudconfig/SpringCloudConfigApplication.java
@@ -0,0 +1,18 @@
+package com.wmm.springcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * {@link EnableConfigServer}激活对配置中心的支持
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigApplication.class, args);
+    }
+
+}
