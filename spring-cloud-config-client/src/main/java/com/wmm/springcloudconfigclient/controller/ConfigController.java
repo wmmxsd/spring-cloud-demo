9e842b3541f1800d335ba7bc2c4b73b297a6f21e
wmmxsd@126.com <w7610232mm>
2020-07-01 18:22:29 +0800

    use Spring Cloud Bus to broadcast config changed event

--- spring-cloud-config-client/src/main/java/com/wmm/springcloudconfigclient/controller/ConfigController.java
+++ spring-cloud-config-client/src/main/java/com/wmm/springcloudconfigclient/controller/ConfigController.java
@@ -1,15 +1,18 @@
 package com.wmm.springcloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * {@link RefreshScope}加上该注解后必须先将Spring Cloud Config Server启动后再启动该工程
  * @author wangmingming
  * @Description 配置控制层
  * @date @2020/6/30 11:46
  */
 @RestController
+@RefreshScope
 public class ConfigController {
     @Value("${config.source}")
     private String source;
